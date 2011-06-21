128cb9ccc805e034a6c53a94a1d8be52a4d69197
Saagar <saagarvarma18@gmail.com>
2011-06-21 17:55:36 +0530

    Update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Centraltime::Application.config.secret_token = '4b9743a3a8fd49c8863ec69b84cd54db4b294287ae06b028381cb8d9502bd10093391035aa41be93e1caf2b0662340707aa55950336eb3626b5e3baf5bc4e4f9'
